b0495f579535231dc6a618d47ea37a0ea6bd3bcf
monalizaloren1 <99450087+monalizaloren1@users.noreply.github.com>
2022-11-28 16:23:32 -0300

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,11 @@
+
+function addUser() {
+
+  userName = document.getElementById("userName").value;
+// Armazenado local do navegador
+// Variavel que guarda o nome do usuário
+  ????.setItem("userName", ???);
+  //Código que muda de tela
+    ???? = "kwitterRoom.html";
+}
+
